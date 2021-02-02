80c1427d0a57066f31d91de6ebc5c3101dae18cc
rishsol <rishabsolanki09@gmail.com>
2021-02-02 11:14:00 -0500

    updated file handling

--- app.py
+++ app.py
@@ -13,22 +13,37 @@ def index():
     return render_template('clean.html')
 
 @app.route('/clean', methods=['POST'])
-def create_task():
+def clean_file():
     nullValue = request.form.get('nullValues')
     interpolation = request.form.get('interpolation')
     file = request.files['file']
 
-    df = pd.read_csv(file)
+    mimetype = file.content_type
+    
+    if mimetype == 'text/csv':
+      df = pd.read_csv(file)
+    else:
+      df = pd.read_excel(file)
+
     if nullValue:
       df.dropna(inplace=True)
 
     df.interpolate(method=interpolation, axis=0, inplace=True)
-    df.to_csv('./data.csv')
-    
-    return send_file('./data.csv',
+
+    if mimetype == 'text/csv':
+      df.to_csv('./data.csv')
+      return send_file('./data.csv',
                      mimetype='text/csv',
                      attachment_filename='data.csv',
                      as_attachment=True)
+    else:
+      df.to_excel('./data.xlsx')
+      return send_file('./data.xlsx',
+                     mimetype='text/xlsx',
+                     attachment_filename='data.xlsx',
+                     as_attachment=True)
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
